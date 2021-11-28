aa64b6bdf75fc94b54c1b75c5c4450d505497865
StudyCrec <80320999+StudyCrec123@users.noreply.github.com>
2021-11-28 15:32:17 +0400

    Create Area and Perimeter of a triangle.py
    Basic formulas for area and perimeter of triangle

---
+++ Area and Perimeter of a triangle.py
@@ -0,0 +1,9 @@
+#Formulas for area of triangle and perimeter of triangle
+base= float(input("Enter the base of the triangle: "))
+height= float(input("Enter the height of the triangle: "))
+hypotenuse= float(input("Enter the hypotenuse of the triangle: "))
+area= 1/2 * base * height
+perimeter= base + height + hypotenuse
+print("Area of triangle:", area)
+print("Perimeter of triangle:", perimeter)
+
